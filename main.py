d219ac7fa7b1ffc57d4791478a3e43d4dff20ac7
12arakawa <haruni.arakawa@gmail.com>
2022-03-31 17:28:01 +0900

    htmlを表示できるようにする

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-# -*- coding: utf-8 -*- 
-from flask import Flask, render_template, request 
-import os 
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
+import os
 
-app = Flask(__name__, static_folder='.', static_url_path='') 
+app = Flask(__name__, static_folder='./sources', static_url_path='/sources')
 
-@app.route('/') 
-def index(): 
-    return "荒川歩夢" 
+@app.route('/')
+def index():
+    # return "Hello World!"
+    return app.send_static_file('html/index.html')
 
-if __name__ == '__main__': 
-    #port = int(os.getenv("PORT")) 
-    app.run(debug=True)
+if __name__ == '__main__':
+    # port = int(os.getenv("PORT"))
+    port = 100
+    app.run(debug=True, host='0.0.0.0', port=port)
